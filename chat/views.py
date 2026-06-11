4fe3accd4f41e5cb378380de8d599205b16a46b1
abinesh-yt <neshabinesh2008@gmail.com>
2026-06-11 22:21:40 +0530

    Add ChatGPT Jr AI chatbot with Groq integration

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,41 @@
 from django.shortcuts import render
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
 
 def home(request):
-    return render(request, 'chat/home.html')
+
+    user_message = ""
+    bot_response = ""
+
+    if request.method == "POST":
+
+        user_message = request.POST.get("message")
+
+        response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_message
+                }
+            ]
+        )
+
+        bot_response = response.choices[0].message.content
+
+    return render(
+        request,
+        "chat/home.html",
+        {
+            "user_message": user_message,
+            "bot_response": bot_response
+        }
+    )
